196119a3ada051d0009c6c5105b07df2f1adc64a
Kjell Breiland <kjell.breiland@gmail.com>
2023-03-13 12:46:48 -0700

    update main

--- cmd/goscript/main.go
+++ cmd/goscript/main.go
@@ -18,48 +18,31 @@ func main() {
 		panic(err)
 	}
 
-	gs, err := goscript.New(config)
-	if err != nil {
-		panic(err)
-	}
-
 	zapLog, err := zap.NewDevelopment()
 	if err != nil {
 		panic(fmt.Sprintf("zap logging could not be initialized", err))
 	}
-	gs.Logger(zapr.NewLogger(zapLog))
+
+	gs, err := goscript.New(config, zapr.NewLogger(zapLog))
+	if err != nil {
+		panic(err)
+	}
 
 	gs.AddTrigger(&goscript.Trigger{
-		Unique: &goscript.Unique{KillMe: true},
-		//Triggers: []string{"input_button.test_button", "input_boolean.test_toggle", "input_number.test_number"},
-		Triggers:      []string{"input_boolean.test_toggle"},
-		DomainTrigger: []string{"input_boolean"},
-		//Periodic:      goscript.Periodics("* * * * *", ""),
-		//States:        goscript.Entities("input_button.test_button", "input_boolean.test_toggle", "input_number.test_number"),
-		States: nil,
-		Eval:   nil,
-		//Eval:     goscript.Eval(`float(state) > 10`, `state == "on"`),
+		Unique:        &goscript.Unique{KillMe: true},
+		Triggers:      []string{"input_button.test_button"},
+		DomainTrigger: []string{"input_button"},
+		Periodic:      goscript.Periodics("* * * * *", ""),
+		States:        goscript.Entities("input_button.test_button", "input_boolean.test_toggle", "input_number.test_number"),
+		Eval:          nil,
 		Func: func(t *goscript.Task) {
-			//log.Println("flipping")
-			//log.Println(t.States["input_boolean.test_toggle"].State)
 			gs.ServiceChan <- services.NewInputBooleanToggle(services.Targets("input_boolean.test_toggle"))
-			//log.Println("waiting 5 seconds")
-
 			t.Sleep(10 * time.Second)
-
-			//if ok := t.WaitUntil("input_number.test_number", nil, 5*time.Second); !ok {
-			//	return
-			//}
-			//log.Println("flipping at end")
 			gs.ServiceChan <- services.NewInputBooleanToggle(services.Targets("input_boolean.test_toggle"))
-			//log.Println(t.States["input_boolean.test_toggle"].State)
-
 		},
 	})
 
 	err = gs.Connect()
-
-	//gs.GetDomainStates([]string{"button"})
 
 	if err != nil {
 		panic(err)
