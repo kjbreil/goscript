dc1ecfe68f342d25213e468b3947e0425acc9d51
Kjell Breiland <kjell.breiland@gmail.com>
2023-04-06 09:43:53 -0700

    get hass area entities

---
+++ area.go
@@ -0,0 +1,42 @@
+package goscript
+
+import "github.com/kjbreil/hass-ws/model"
+
+func (gs *GoScript) GetAreaDomain(area, domain string) []string {
+	var results []string
+
+	if a, ok := gs.areaRegistry[area]; ok {
+		for _, e := range a {
+			if e.Domain() == domain {
+				results = append(results, e.DomainEntity())
+			}
+		}
+	}
+
+	return results
+}
+
+func (gs *GoScript) fillAreaRegistry() {
+	devices := make(map[string]string)
+	devs := gs.ws.GetDeviceRegistry()
+	for _, dev := range devs.Result {
+		if dev.AreaId != nil {
+			devices[dev.Id] = *dev.AreaId
+		}
+	}
+
+	gs.areaRegistry = make(map[string][]model.Result)
+	entities := gs.ws.GetEntityRegistry()
+	for _, entity := range entities.Result {
+		if entity.AreaId != nil {
+			gs.areaRegistry[*entity.AreaId] = append(gs.areaRegistry[*entity.AreaId], entity)
+			continue
+		}
+		if entity.DeviceId != nil {
+			areaId, ok := devices[*entity.DeviceId]
+			if ok {
+				gs.areaRegistry[areaId] = append(gs.areaRegistry[areaId], entity)
+			}
+		}
+	}
+}
