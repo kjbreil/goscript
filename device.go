0f2df97cedf92f82de076e03bb949f1a541ff5cf
Kjell Breiland <kjell.breiland@gmail.com>
2023-04-27 09:01:29 -0700

    make device in map

--- device.go
+++ device.go
@@ -1,18 +1,20 @@
 package goscript
 
 import (
+	"fmt"
 	"github.com/kjbreil/hass-mqtt/device"
 	"github.com/kjbreil/hass-mqtt/entities"
 )
 
 type Device struct {
 	dev      *device.Device
-	entities []entities.Entity
+	entities map[string]entities.Entity
 }
 
 func (gs *GoScript) AddDevice(dev *device.Device) (*Device, error) {
 	d := &Device{
-		dev: dev,
+		dev:      dev,
+		entities: make(map[string]entities.Entity),
 	}
 
 	err := gs.mqtt.Add(d.dev)
@@ -25,11 +27,19 @@ func (gs *GoScript) AddDevice(dev *device.Device) (*Device, error) {
 	return d, nil
 }
 
-func (d *Device) AddEntities(ets []entities.Entity) error {
-	d.entities = append(d.entities, ets...)
-	for _, et := range ets {
-		err := d.dev.Add(et)
+func (gs *GoScript) GetDevice(entity string) (*Device, error) {
+	d, ok := gs.devices[entity]
+	if !ok {
+		return nil, fmt.Errorf("could not find device %s", entity)
+	}
 
+	return d, nil
+}
+
+func (d *Device) AddEntities(ets []entities.Entity) error {
+	for _, et := range ets {
+		d.entities[et.GetDomainEntity()] = et
+		err := d.dev.Add(et)
 		if err != nil {
 			return err
 		}
@@ -38,7 +48,19 @@ func (d *Device) AddEntities(ets []entities.Entity) error {
 }
 
 func (d *Device) GetEntities() []entities.Entity {
-	return d.entities
+	ets := make([]entities.Entity, 0, len(d.entities))
+	for _, et := range d.entities {
+		ets = append(ets, et)
+	}
+	return ets
+}
+
+func (d *Device) GetEntity(domainEntity string) entities.Entity {
+	e, ok := d.entities[domainEntity]
+	if !ok {
+		return nil
+	}
+	return e
 }
 func (d *Device) GetUniqueID() string {
 	return d.dev.GetUniqueId()
